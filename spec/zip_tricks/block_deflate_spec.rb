31c751757f57ad4e9d325f186aaf0b1b4bc06691
Julik Tarkhanov <me@julik.nl>
2015-10-13 01:51:28 +0200

    Another spec sketch

--- spec/zip_tricks/block_deflate_spec.rb
+++ spec/zip_tricks/block_deflate_spec.rb
@@ -6,6 +6,7 @@ describe ZipTricks::BlockDeflate do
     it 'removes the header'
     it 'removes the adler32'
     it 'removes the end marker'
+    it 'honors the level'
   end
   
   describe 'deflate_in_blocks_and_terminate' do
@@ -14,8 +15,13 @@ describe ZipTricks::BlockDeflate do
     it 'writes the end marker without the adler32'
   end
   
+  describe '.write_terminator' do
+    it 'writes the terminator and returns 2 for number of bytes written'
+  end
+  
   describe '.deflate_in_blocks' do
     it 'honors the block size'
+    it 'honors the level and passes it to deflate_chunk'
     it 'produces a blob that can be inflated later'
     it 'does not write the end marker'
   end

bd88dcf088dbf2ae6de46488e23054a8ae9d4593
Julik Tarkhanov <me@julik.nl>
2015-10-20 10:04:36 +0200

    Upver

--- lib/zip_tricks.rb
+++ lib/zip_tricks.rb
@@ -1,6 +1,6 @@
 require 'zip'
 module ZipTricks
-  VERSION = '2.1.1'
+  VERSION = '2.1.2'
   
   # Require all the sub-components except myself
   Dir.glob(__dir__ + '/**/*.rb').each {|p| require p unless p == __FILE__ }

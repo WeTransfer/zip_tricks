45eec7265ee9a2137b5ca3db29878e2be38500e1
Julik Tarkhanov <me@julik.nl>
2015-11-01 11:33:14 +0100

    Spec for WriteAndTell#advance_position_by

--- spec/zip_tricks/write_and_tell_spec.rb
+++ spec/zip_tricks/write_and_tell_spec.rb
@@ -2,7 +2,6 @@ require_relative '../spec_helper'
 
 describe ZipTricks::WriteAndTell do
   it 'maintains the count of bytes written' do
-    blobs = []
     adapter = described_class.new('')
     expect(adapter.tell).to be_zero
     
@@ -10,5 +9,20 @@ describe ZipTricks::WriteAndTell do
     adapter << ''
     adapter << '!'
     expect(adapter.tell).to eq(6)
+  end
+  
+  it 'advances the internal pointer using advance_position_by' do
+    str = ''
+    
+    adapter = described_class.new(str)
+    expect(adapter.tell).to be_zero
+    
+    adapter << 'hello'
+    adapter << ''
+    adapter << '!'
+    expect(adapter.tell).to eq(6)
+    adapter.advance_position_by(128981)
+    expect(adapter.tell).to eq(6 + 128981)
+    expect(str).to eq('hello!')
   end
 end

4f10a61c46d9363dd6b2f06a8c8591ecf70495cc
Julik Tarkhanov <me@julik.nl>
2016-03-06 15:32:38 +0100

    Add a global NullWriter module

---
+++ lib/zip_tricks/null_writer.rb
@@ -0,0 +1,7 @@
+# Used when you need to supply a destination IO for some
+# write operations, but want to discard the data (like when
+# estimating the size of a ZIP)
+module ZipTricks::NullWriter
+  def <<(data); self; end
+  extend self
+end

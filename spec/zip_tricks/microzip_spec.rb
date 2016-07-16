c5f9cf2acbd657c2e2e0877e2e77d1f5fa5a90cb
Julik Tarkhanov <me@julik.nl>
2016-07-16 12:47:41 +0200

    Add a test with an obscene number of files in the ZIP

--- spec/zip_tricks/microzip_spec.rb
+++ spec/zip_tricks/microzip_spec.rb
@@ -24,11 +24,11 @@ describe ZipTricks::Microzip do
   it 'creates an archive with a number of very tiny text files' do
     tf = Tempfile.new('zip')
     z = described_class.new(tf)
-    
+
     test_str = Random.new.bytes(64)
     crc = Zlib.crc32(test_str)
     t = Time.now.utc
-    
+
     13.times do |i|
       fn = "test-#{i}.bin"
       z.add_local_file_header(filename: fn, crc32: crc, compressed_size: test_str.bytesize,
@@ -37,9 +37,9 @@ describe ZipTricks::Microzip do
     end
     z.write_central_directory
     tf.flush
-    
-    entries = []
+
     Zip::File.open(tf.path) do |zip_file|
+      entries = []
       zip_file.each do |entry|
         entries << entry
         readback = entry.get_input_stream.read
@@ -48,6 +48,35 @@ describe ZipTricks::Microzip do
         expect(entry.name).to match(/test/)
       end
       expect(entries.length).to eq(13)
+    end
+  end
+
+  it 'creates an archive with more than 0xFFFF file entries (Zip64 due to number of files)' do
+    tf = Tempfile.new('zip')
+    z = described_class.new(tf)
+
+    test_str = Random.new.bytes(64)
+    crc = Zlib.crc32(test_str)
+    t = Time.now.utc
+
+    n_files = 0xFFFF + 6
+
+    n_files.times do |i|
+      fn = "test-#{i}.bin"
+      z.add_local_file_header(filename: fn, crc32: crc, compressed_size: test_str.bytesize,
+        uncompressed_size: test_str.bytesize, storage_mode: 0, mtime: t)
+      tf << test_str
+    end
+    z.write_central_directory
+    tf.flush
+
+    Zip::File.open(tf.path) do |zip_file|
+      entries = []
+      zip_file.each do |entry|
+        entries << entry
+        expect(entry.name).to match(/test/)
+      end
+      expect(entries.length).to eq(n_files)
     end
   end
 end

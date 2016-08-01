0e569f94479ffbf4a574133cccf639e03770b8de
Julik Tarkhanov <me@julik.nl>
2016-08-01 12:22:35 +0200

    And update this part too

--- spec/zip_tricks/size_estimator_spec.rb
+++ spec/zip_tricks/size_estimator_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../spec_helper'
 
-describe ZipTricks::StoredSizeEstimator do
+describe ZipTricks::SizeEstimator do
   it 'accurately predicts the output zip size' do
     # Generate a couple of random files
     raw_file_1 = SecureRandom.random_bytes(1024 * 20)
     raw_file_2 = SecureRandom.random_bytes(1024 * 128)
     raw_file_3 = SecureRandom.random_bytes(1258695)
 
-    predicted_size = described_class.perform_fake_archiving do | estimator |
+    predicted_size = described_class.estimate do | estimator |
       r = estimator.add_stored_entry(filename: "first-file.bin", size: raw_file_1.size)
       expect(r).to eq(estimator), "add_stored_entry should return self"
 

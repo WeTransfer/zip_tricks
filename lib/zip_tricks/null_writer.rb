c417836481c1420db8c9a5afe36538008683de3d
Julik Tarkhanov <me@julik.nl>
2016-08-03 19:09:40 +0200

    Note that it returns self basically

--- lib/zip_tricks/null_writer.rb
+++ lib/zip_tricks/null_writer.rb
@@ -2,6 +2,7 @@
 # write operations, but want to discard the data (like when
 # estimating the size of a ZIP)
 module ZipTricks::NullWriter
+  # @return [self]
   def <<(data); self; end
   extend self
 end

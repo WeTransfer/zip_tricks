6ee080f4d7a7a2d8bf9028ec3fd452c1d9abd8c2
Julik Tarkhanov <me@julik.nl>
2016-11-14 00:54:15 +0100

    Bump the version

--- lib/zip_tricks.rb
+++ lib/zip_tricks.rb
@@ -1,5 +1,5 @@
 module ZipTricks
-  VERSION = '4.2.1'
+  VERSION = '4.2.2'
   
   # Require all the sub-components except myself
   Dir.glob(__dir__ + '/**/*.rb').sort.each {|p| require p unless p == __FILE__ }

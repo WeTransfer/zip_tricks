f5cffbffd89e0385b6d63aa4ef9c4b4d996c111b
Julik Tarkhanov <me@julik.nl>
2017-04-04 12:13:11 +0200

    Improve documentation generation for text/Markdown
    and document the Rails module

--- Rakefile
+++ Rakefile
@@ -3,7 +3,9 @@ require "rspec/core/rake_task"
 require 'yard'
 
 YARD::Rake::YardocTask.new(:doc) do |t|
-  t.files = ['lib/**/*.rb', 'README.md', 'LICENSE.txt', 'IMPLEMENTATION_DETAILS.md']
+  # The dash has to be between the two to "divide" the source files and 
+  # miscellaneous documentation files that contain no code
+  t.files = ['lib/**/*.rb', '-', 'LICENSE.txt', 'IMPLEMENTATION_DETAILS.md']
 end
 
 RSpec::Core::RakeTask.new(:spec)
--- lib/zip_tricks/rails_streaming.rb
+++ lib/zip_tricks/rails_streaming.rb
@@ -1,4 +1,10 @@
+# Should be included into a Rails controller (together with `ActionController::Live`)
+# for easy ZIP output from any action.
 module ZipTricks::RailsStreaming
+  # Opens a {ZipTricks::Streamer} and yields it to the caller. The output of the streamer
+  # gets automatically forwarded to the Rails response stream. When the output completes,
+  # the Rails response stream is going to be closed automatically.
+  # @yield [Streamer] the streamer that can be written to
   def zip_tricks_stream
     response.headers['Content-Type'] = 'application/zip'
     # Create a wrapper for the write call that quacks like something you

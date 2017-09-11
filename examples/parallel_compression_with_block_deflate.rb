483c1145f2eef4302aadc6ca940eeb32a5843235
David Bosveld <david@Davids-MacBook-Pro.local>
2017-09-11 10:48:08 +0200

    removed trailing white space and added a space after map function

--- examples/parallel_compression_with_block_deflate.rb
+++ examples/parallel_compression_with_block_deflate.rb
@@ -21,21 +21,21 @@ threads = (0..12).map do
   Thread.new do
     source_tempfile = Tempfile.new 't'
     source_tempfile.binmode
-  
+
     # Fill the part with random content
     12.times { source_tempfile << Random.new.bytes(1 * 1024 * 1024) }
     source_tempfile.rewind
-  
+
     # Compute the CRC32 of the source file
     part_crc = ZipTricks::StreamCRC32.from_io(source_tempfile)
     source_tempfile.rewind
-  
+
     # Create a compressed part
     compressed_tempfile = Tempfile.new('tc')
     compressed_tempfile.binmode
     ZipTricks::BlockDeflate.deflate_in_blocks(source_tempfile,
                                               compressed_tempfile)
-    
+
     source_tempfile.close!
     # The data that the splicing process needs.
     [compressed_tempfile, part_crc, source_tempfile.size]
@@ -62,7 +62,7 @@ ZipTricks::BlockDeflate.write_terminator(last_compressed_part)
 # To figure that out we just sum the sizes of the files
 compressed_part_files = compressed_tempfiles_and_crc_of_parts.map(&:first)
 size_of_deflated_segment = compressed_part_files.map(&:size).inject(&:+)
-size_of_uncompressed_file = compressed_tempfiles_and_crc_of_parts.map{ |e| e[2] }.inject(&:+)
+size_of_uncompressed_file = compressed_tempfiles_and_crc_of_parts.map { |e| e[2] }.inject(&:+)
 
 # And now we can create a ZIP with our compressed file in it's entirety.
 # We use a File as a destination here, but you can also use a socket or a
@@ -71,10 +71,10 @@ size_of_uncompressed_file = compressed_tempfiles_and_crc_of_parts.map{ |e| e[2] 
 output = File.open('zip_created_in_parallel.zip', 'wb')
 
 ZipTricks::Streamer.open(output) do |zip|
-  zip.add_compressed_entry( 'parallel.bin',
-                            size_of_uncompressed_file,
-                            entire_file_crc.to_i,
-                            size_of_deflated_segment)
+  zip.add_compressed_entry('parallel.bin',
+                           size_of_uncompressed_file,
+                           entire_file_crc.to_i,
+                           size_of_deflated_segment)
   compressed_part_files.each do |part_file|
     part_file.rewind
     while blob = part_file.read(2048)

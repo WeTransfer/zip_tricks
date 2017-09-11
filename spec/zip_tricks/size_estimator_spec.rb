4e9c3b5344d9ed0a88669367385911510b9748cb
David Bosveld <david@Davids-MacBook-Pro.local>
2017-09-11 13:41:50 +0200

    Changed number variable into literal

--- spec/zip_tricks/size_estimator_spec.rb
+++ spec/zip_tricks/size_estimator_spec.rb
@@ -3,29 +3,29 @@ require_relative '../spec_helper'
 describe ZipTricks::SizeEstimator do
   it 'accurately predicts the output zip size' do
     # Generate a couple of random files
-    raw_file_1 = Random.new.bytes(1_024 * 20)
-    raw_file_2 = Random.new.bytes(1_024 * 128)
-    raw_file_3 = Random.new.bytes(1_258_695)
+    raw_file_one = Random.new.bytes(1_024 * 20)
+    raw_file_two = Random.new.bytes(1_024 * 128)
+    raw_file_three = Random.new.bytes(1_258_695)
 
     predicted_size = described_class.estimate do |estimator|
-      r = estimator.add_stored_entry(filename: 'first-file.bin', size: raw_file_1.size)
+      r = estimator.add_stored_entry(filename: 'first-file.bin', size: raw_file_one.size)
       expect(r).to eq(estimator), 'add_stored_entry should return self'
 
-      estimator.add_stored_entry(filename: 'second-file.bin', size: raw_file_2.size)
+      estimator.add_stored_entry(filename: 'second-file.bin', size: raw_file_two.size)
 
       r = estimator.add_compressed_entry(filename: 'second-file.bin',
-                                         compressed_size: raw_file_3.size,
-                                         uncompressed_size: raw_file_2.size)
+                                         compressed_size: raw_file_three.size,
+                                         uncompressed_size: raw_file_two.size)
       expect(r).to eq(estimator), 'add_compressed_entry should return self'
 
       r = estimator.add_stored_entry(filename: 'first-file-with-descriptor.bin',
-                                     size: raw_file_1.size,
+                                     size: raw_file_one.size,
                                      use_data_descriptor: true)
       expect(r).to eq(estimator), 'add_stored_entry should return self'
 
       r = estimator.add_compressed_entry(filename: 'second-file-with-descriptor.bin',
-                                         compressed_size: raw_file_3.size,
-                                         uncompressed_size: raw_file_2.size,
+                                         compressed_size: raw_file_three.size,
+                                         uncompressed_size: raw_file_two.size,
                                          use_data_descriptor: true)
       expect(r).to eq(estimator), 'add_compressed_entry should return self'
 

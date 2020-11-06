bcba5e007ac5b40c891e45bc41dc2a1d0b3095c2
Julik Tarkhanov <me@julik.nl>
2020-11-06 13:12:33 +0100

    Update OutputEnumerator documentation

--- lib/zip_tricks/output_enumerator.rb
+++ lib/zip_tricks/output_enumerator.rb
@@ -1,31 +1,37 @@
 # frozen_string_literal: true
 
-# Can be used as a Rack response body directly. Will yield
-# a {ZipTricks::Streamer} for adding entries to the archive and writing
-# zip entry bodies.
+# The output enumerator makes it possible to "pull" from a ZipTricks streamer
+# object instead of having it "push" writes to you. It will "stash" the block which
+# writes the ZIP archive through the streamer, and when you call `each` on the Enumerator
+# it will yield you the bytes the block writes. Since it is an enumerator you can
+# use `next` to take chunks written by the ZipTricks streamer one by one. It can be very
+# convenient when you need to segment your ZIP output into bigger chunks for, say,
+# uploading them to a cloud storage provider such as S3.
+#
+# Another use of the output enumerator is outputting a ZIP archive from Rails or Rack,
+# where an object responding to `each` is required which yields Strings. For instance,
+# you can return a ZIP archive from Rack like so:
+#
+#     iterable_zip_body = ZipTricks::OutputEnumerator.new do | streamer |
+#       streamer.write_deflated_file('big.csv') do |sink|
+#         CSV(sink) do |csv_write|
+#           csv << Person.column_names
+#           Person.all.find_each do |person|
+#             csv << person.attributes.values
+#           end
+#         end
+#       end
+#     end
+#
+#     [200, {'Content-Type' => 'binary/octet-stream'}, iterable_zip_body]
 class ZipTricks::OutputEnumerator
-  # Prepares a new Rack response body with a Zip output stream.
-  # The block given to the constructor will be called when the response
-  # body will be read by the webserver, and will receive a {ZipTricks::Streamer}
-  # as it's block argument. You can then add entries to the Streamer as usual.
-  # The archive will be automatically closed at the end of the block.
+  # Creates a new OutputEnumerator.
   #
-  #     # Precompute the Content-Length ahead of time
-  #     content_length = ZipTricks::SizeEstimator.estimate do | estimator |
-  #       estimator.add_stored_entry(filename: 'large.tif', size: 1289894)
-  #     end
-  #
-  #     # Prepare the response body.
-  #     # The block will only be called when the
-  #     # response starts to be written.
-  #     body = ZipTricks::OutputEnumerator.new do | streamer |
-  #       streamer.add_stored_entry(filename: 'large.tif', size: 1289894, crc32: 198210)
-  #       streamer << large_file.read(1024*1024) until large_file.eof?
-  #       ...
-  #     end
-  #
-  #     return [200, {'Content-Type' => 'binary/octet-stream',
-  #     'Content-Length' => content_length.to_s}, body]
+  # @param streamer_options[Hash] options for Streamer, see {ZipTricks::Streamer.new}
+  #     It might be beneficial to tweak the `write_buffer_size` to your liking so that you won't be
+  #     doing too many write attempts and block right after
+  # @param blk a block that will receive the Streamer object when executing. The block will not be executed
+  #     immediately but only once `each` is called on the OutputEnumerator
   def initialize(**streamer_options, &blk)
     @streamer_options = streamer_options.to_h
     @archiving_block = blk
@@ -33,7 +39,10 @@ class ZipTricks::OutputEnumerator
 
   # Executes the block given to the constructor with a {ZipTricks::Streamer}
   # and passes each written chunk to the block given to the method. This allows one
-  # to "take" output of the ZIP piecewise.
+  # to "take" output of the ZIP piecewise. If called without a block will return an Enumerator
+  # that you can pull data from using `next`.
+  #
+  # @yield [String] a chunk of the ZIP output in binary encoding
   def each
     if block_given?
       block_write = ZipTricks::BlockWrite.new { |chunk| yield(chunk) }
